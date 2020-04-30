8d486ea7316edcbbca91ef2f8f2bad0154f4edd9
ML5W3S <marcelolauriano@gmail.com>
2020-04-30 14:33:18 -0300

    Novos arquivos
    Arquivos para buscar o menor número de linhas de código

---
+++ readme.md.txt
@@ -0,0 +1,5 @@
+Opa
+Repositório para desenvolvimento de menu feitos em HTML5 e CSS3
+
+Hi
+Repository for menu development made in HTML5 and CSS3
---
+++ script.js
@@ -0,0 +1,21 @@
+/*var troca = 1;
+if (troca == 0){
+     document.write(document.getElementById("oculta");
+}else{
+document.write ("Opa");
+}
+var veri = 1;
+function myFunction() {
+  document.getElementById("menu-hidde").innerHTML = "Menu";
+}
+ var trigger = document.getElementById('menu-trigger').addEventListener("click"function(){
+ var menu = document.getElementById('menu-hidde');
+ if (veri == 1) {
+ }
+ menu.style.left = "0px";
+ veri = 0;
+ }else{
+ menu.style.left = "100%";
+ veri = 1;
+ }
+ })*/
